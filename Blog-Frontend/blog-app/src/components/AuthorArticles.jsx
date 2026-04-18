b9e89401b57ee6c2d184189d166a60709519e4db
abhiii966 <24eg105j32@anurag.edu.in>
2026-04-18 16:52:59 +0530

    endpoint updates

--- Blog-Frontend/blog-app/src/components/AuthorArticles.jsx
+++ Blog-Frontend/blog-app/src/components/AuthorArticles.jsx
@@ -35,7 +35,7 @@ function AuthorArticles() {
       try {
       setLoading(true);
        //read articles of current author
-      let res=await axios.get("http://localhost:6677/author-api/articles",{withCredentials:true})
+      let res=await axios.get("https://two4eg105j32-project1.onrender.com/author-api/articles",{withCredentials:true})
       if(res.status===200){
         setArticles(res.data.payload);
       }
